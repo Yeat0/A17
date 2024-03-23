479d44ed4ab2a3fb79316ececa11a7cd6416c2ff
Yeat0 <147911103+Yeat0@users.noreply.github.com>
2024-03-23 23:46:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,11 +21,11 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["916297175943", "919362639362"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["916297175943", "919362639362"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["916297175943"];
-global.OwnerName = "Kai";
-global.BotName = "A17";
+global.Owner = ["27698908814"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["27698908814"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["27698908814"];
+global.OwnerName = "Mikyle";
+global.BotName = "coco-md";
 global.packname = "A17 Bot";                             //Do not change.
 global.author = "By: Kai";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai0071/A17"; //Do not change.
